ae4ceeda932c4a703d6f7a4439b408d8a80f8d64
Kyle Getrost <kyle@motionhungry.com>
2024-02-02 12:43:48 +0700

    feat: add strapi Dockerfile

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
-import fs from "fs";
-import { fileURLToPath } from "url";
-import path from "path";
-import { program } from "commander";
-import { $, ExecaReturnValue } from "execa";
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { program } from 'commander';
+import { $, ExecaReturnValue } from 'execa';
 
-type PackageManager = "bun" | "npm" | "pnpm" | "yarn";
+type PackageManager = 'bun' | 'npm' | 'pnpm' | 'yarn';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const copyTemplateFile = async (
   srcFile: string,
-  destFile: string
+  destFile: string,
 ): Promise<ExecaReturnValue> => {
   console.log(`Adding ${destFile}...`);
   const source = path.join(__dirname, `templates/${srcFile}`);
@@ -25,54 +25,69 @@ const fileExists = (filename: string) => {
   return fs.existsSync(path.join(process.cwd(), filename));
 };
 
+const getPackageManager = (): PackageManager => {
+  if (fileExists('pnpm-lock.yaml')) {
+    return 'pnpm';
+  }
+  if (fileExists('yarn.lock')) {
+    return 'yarn';
+  }
+  if (fileExists('bun.lockb')) {
+    return 'bun';
+  }
+  return 'npm';
+};
+
 const installPackages = async (
   packages: string[],
-  isDev: boolean = false
+  isDev: boolean = false,
 ): Promise<ExecaReturnValue> => {
   console.log(`Installing ${packages.join(' ')}...`);
   const args = [];
-
-  const command: PackageManager = (() => {
-    if (fileExists("pnpm-lock.yaml")) {
-      return "pnpm";
-    }
-    if (fileExists("yarn.lock")) {
-      return "yarn";
-    }
-    if (fileExists("bun.lockb")) {
-      return "bun";
-    }
-    return "npm";
-  })();
+  const command = getPackageManager();
 
   if (isDev) {
-    if (["pnpm", "yarn"].includes(command)) {
-      args.push("-D");
-    } else if (command === "bun") {
+    if (['pnpm', 'yarn'].includes(command)) {
+      args.push('-D');
+    } else if (command === 'bun') {
       args.push('-d');
     } else {
-      " --save-dev "
+      (' --save-dev ');
     }
   }
 
   return await $`${command} add ${[...args, ...packages]}`;
 };
 
-const itemMap: Record<string, () => Promise<void>> = {
-  prettier: async () => {
-    copyTemplateFile("prettier/.prettierrc", ".prettierrc");
-    installPackages(["@motionhungry/style-policy"], true);
-  },
+function prettier() {
+  copyTemplateFile('prettier/.prettierrc', '.prettierrc');
+  installPackages(['@motionhungry/style-policy'], true);
+}
+
+function strapi() {
+  copyTemplateFile('strapi/Dockerfile', 'Dockerfile');
+  prettier();
+}
+
+const itemMap = {
+  prettier,
+  strapi,
 };
 
 program
-  .command("generate")
-  .argument("<item>", "item that you want to generate")
+  .command('generate')
+  .argument('<item>', 'item that you want to generate')
   .action((item: keyof typeof itemMap) => {
     const func = itemMap[item];
     if (func) {
       func();
     }
   });
+
+program.command('list').action(() => {
+  Object.keys(itemMap).forEach((item) => {
+    console.log(item);
+  });
+});
 
 program.parse();
